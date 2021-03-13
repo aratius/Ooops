bd7712f2c6743ed4f3a852cc727a4cd354fd16c1
aratius <arata1129matsu@icloud.com>
2021-03-13 17:03:01 +0900

    画面外に出たときに削除

---
+++ Assets/Scripts/BoundaryScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryScript : MonoBehaviour
+{
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
