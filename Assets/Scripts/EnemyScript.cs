918e51a16639f623fb24bfbf8f3b8a6f1f25aa1e
aratius <arata1129matsu@icloud.com>
2021-03-13 17:57:41 +0900

    敵を倒すと得点を加算

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -7,11 +7,15 @@ public class EnemyScript : MonoBehaviour
 
     private float phase;  //三角関数の位相をバラつかせるため
     public GameObject explosion;
+    private GameControllerScript gameController;
 
     // Start is called before the first frame update
     void Start()
     {
         phase = Random.Range(0f, Mathf.PI * 2);
+        gameController = GameObject
+            .FindWithTag("GameController")
+            .GetComponent<GameControllerScript>();
     }
 
     // Update is called once per frame
@@ -22,13 +26,16 @@ public class EnemyScript : MonoBehaviour
             -2f * Time.deltaTime,
             0f);
     }
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        // 弾と衝突
         if (other.gameObject.CompareTag("Bullet")) {
+            gameController.AddScore(10);  //得点を加算
             Instantiate(explosion, transform.position, transform.rotation);
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
+        // プレイヤーと衝突
         if (other.gameObject.CompareTag("Player")) {
             Instantiate(explosion, other.transform.position, other.transform.rotation);
             Destroy(other.gameObject);
